9936546ca5ba9a69178e0c78afb80d7da1eb7a84
Link <mrchengzp@qq.com>
2026-01-14 16:37:37 +0800

    feat: add grafana monitor

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 import './index.css';
 import { App } from './App';
+
+initializeFaro({
+  url: 'https://faro-collector-prod-ap-southeast-1.grafana.net/collect/d730ce3555958ea089459acd1cd6886b',
+  app: {
+    name: 'stock-dashboard',
+    version: '1.0.0',
+    environment: 'production'
+  },
+  
+  instrumentations: [
+    ...getWebInstrumentations(),
+    new TracingInstrumentation(),
+  ],
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
