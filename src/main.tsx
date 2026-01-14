cebe24f591d2aa4643101cd789ce36be7df340d3
Link <mrchengzp@qq.com>
2026-01-14 17:45:21 +0800

    chore: report

--- src/main.tsx
+++ src/main.tsx
@@ -11,27 +11,30 @@ import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 import './index.css';
 import { App } from './App';
 
-initializeFaro({
-  url: 'https://faro-collector-prod-ap-southeast-1.grafana.net/collect/d730ce3555958ea089459acd1cd6886b',
-  app: {
-    name: 'stock-dashboard',
-    version: '1.0.0',
-    environment: 'production'
-  },
 
-  instrumentations: [
-    ...getWebInstrumentations(),
-    new TracingInstrumentation(),
-    new ReactIntegration({
-      router: createReactRouterV6DataOptions({
-        matchRoutes,
-      }),
-    }),
-  ],
-});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+if (import.meta.env.PROD) {
+  initializeFaro({
+    url: 'https://faro-collector-prod-ap-southeast-1.grafana.net/collect/d730ce3555958ea089459acd1cd6886b',
+    app: {
+      name: 'stock-dashboard',
+      version: '1.0.0',
+      environment: 'production',
+    },
+
+    instrumentations: [
+      ...getWebInstrumentations(),
+      new TracingInstrumentation(),
+      new ReactIntegration({
+        router: createReactRouterV6DataOptions({
+          matchRoutes,
+        }),
+      }),
+    ],
+  });
+}
